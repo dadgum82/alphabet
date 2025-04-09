eef533d85fc4b90846d613e26d62b3a04b2f81b0
dadgum82 <93083293+dadgum82@users.noreply.github.com>
2025-04-09 08:33:51 -0400

    Start of project

---
+++ src/components/AlphabetDisplay.tsx
@@ -0,0 +1,20 @@
+// src/components/AlphabetDisplay.tsx
+import React from 'react';
+import styles from '../styles/AlphabetDisplay.module.css';
+
+const AlphabetDisplay: React.FC = () => {
+    return (
+        <div className={styles.alphabetContainer}>
+            <h1>Alphabet Display</h1>
+            <div className={styles.letterContainer}>
+                {/* Team Member 1: Implement A-E letters */}
+                {/* Team Member 2: Implement F-J letters */}
+                {/* Team Member 3: Implement K-O letters */}
+                {/* Team Member 4: Implement P-T letters */}
+                {/* Team Member 5: Implement U-Z letters */}
+            </div>
+        </div>
+    );
+};
+
+export default AlphabetDisplay;
